2049b7da8b5df0579b9c6da7171fa51be4899055
Maikol  Campos <Camposmaikol1@gmail.com>
2024-10-22 18:39:27 -0400

    Nuevo Commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,22 @@
+$(document).ready(function () {
 
+    $("#flag-us").click(function () { 
 
+        const src = $(this).attr("src");
+        if(src.endsWith("us-flag.svg")){
+
+            $(this).attr("src", "img/es-flag.svg");
+
+            //Codigo para cambiar textos a ingles
+
+        }else{
+
+            $(this).attr("src", "img/us-flag.svg");
+
+            //Codigo para cambiar textos a espannol
+
+        }
+
+     });
+
+});
