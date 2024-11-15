cfda75709ae96433f1244f2d9c4e369affa3b5e9
Maikol  Campos <Camposmaikol1@gmail.com>
2024-11-14 23:28:52 -0500

    v1.1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+    //Javascript para el boton de cambiar idioma
     $("#flag-us").click(function () { 
 
         const src = $(this).attr("src");
@@ -7,66 +8,198 @@ $(document).ready(function () {
 
             $(this).attr("src", "img/es-flag.svg");
 
-            //Codigo para cambiar textos a ingles
-
         }else{
 
             $(this).attr("src", "img/us-flag.svg");
 
-            //Codigo para cambiar textos a espannol
-
         }
 
      });
-     const destinos = [
-        { provincia: "Matanzas", nombre: "Cuevas de Bellamar", img: "img/Destinos/Matanzas/Cuevas de Bellamar.webp" },
-        { provincia: "Matanzas", nombre: "Cuevas de Saturno", img: "img/Destinos/Matanzas/Cuevas de Saturno.webp" },
-        { provincia: "Matanzas", nombre: "Varadero", img: "img/Destinos/Matanzas/Varadero.webp" },
-        { provincia: "Matanzas", nombre: "Guama", img: "img/Destinos/Matanzas/Guama.webp" },
-        { provincia: "Matanzas", nombre: "Caleta Buena", img: "img/Destinos/Matanzas/Caleta Buena.webp" },
-        { provincia: "La Habana", nombre: "La Habana Vieja", img: "img/Destinos/Habana/Capitolio.webp" },
-        { provincia: "La Habana", nombre: "Malecón", img: "img/Destinos/Habana/Habana Vieja.webp" },
-        { provincia: "La Habana", nombre: "Plaza de la Revolución", img: "img/Destinos/Habana/Morro.webp" },
-        { provincia: "La Habana", nombre: "Capitolio", img: "img/Destinos/Habana/Jardin Botanico.webp" },
-        { provincia: "La Habana", nombre: "Castillo del Morro", img: "img/Destinos/Habana/Malecon.webp" },
+
+     
+     $('.btn-change.prev').hide(); //Ocultar la flecha de la tarjeta hacia atras al cargar la pagina
+
+     $('.btn-change.next').click(function() {
+
+        // Cambiar el contenido de la tarjeta
+        const card = $(this).closest('.card');
+        const newContent = {
+            title: "Tour Dos Ciudades: Cienfuegos y Trinidad desde Varadero",
+            description: "Ver Información",
+            image: "img/Destinos/Tres Ciudades.webp" 
+        };
+
+        // Actualizar el contenido de la tarjeta
+        card.find('h3').text(newContent.title);
+        card.find('.description a').text(newContent.description);
+        card.find('img').attr('src', newContent.image);
+
+        $(this).addClass('rotate-right'); // Agregar la clase de rotación hacia la derecha
+        
+                $(this).hide(); // Ocultar después de que se desvanezca
+                $('.prev').show().removeClass('rotate-left').css('opacity', 1); // Mostrar la flecha anterior
       
-        
-        
-    ];
-
-    function generarTarjetas() {
-        destinos.forEach(destino => {
-            const cardHTML = `
-                <div class="card" data-provincia="${destino.provincia}">
-                    <img src="${destino.img}" alt="${destino.nombre}">
-                    <h3>${destino.nombre}</h3>
-                    <p><a href="">Ver en el mapa</a></p>
-                </div>
-            `;
-            $(".galery").append(cardHTML); // Agrega la tarjeta a la galería
-        });
-    }
-
-    // Generar las tarjetas al cargar la página
-    generarTarjetas();
-
-    $(".galery .card").hide();
-
-    $("input[name='provincia'][value='Matanzas']").prop('checked', true);
-    $(".galery .card[data-provincia='Matanzas']").show();
-
-
-   
-
-    $("input[name='provincia']").change(function() {
-        var selectedValue = $(this).val(); 
-
-        $(".galery .card").hide();
-
-        $(".galery .card[data-provincia='" + selectedValue + "']").show();
     });
 
-   
+    $('.btn-change.prev').click(function() {
+        // Cambiar el contenido de la tarjeta
+        const card = $(this).closest('.card');
+        const newContent = {
+            title: "Tour Tres Ciudades: Villa Clara, Cienfuegos y Trinidad desde Varadero",
+            description: "Ver Información",
+            image: "img/Destinos/Tres Ciudades.webp" 
+        };
+
+        // Actualizar el contenido de la tarjeta
+        card.find('h3').text(newContent.title);
+        card.find('.description a').text(newContent.description);
+        card.find('img').attr('src', newContent.image);
+
+        $(this).addClass('rotate-left'); // Agregar la clase de rotación hacia la izquierda
+       
+                $(this).hide(); // Ocultar después de que se desvanezca
+                $('.next').show().removeClass('rotate-right').css('opacity', 1); // Mostrar la flecha siguiente
+       
+    });
+
+    $('.modal').hide();
+
+     const infoTraslados = {
+        "info1": {
+            title: "Traslado desde el aeropuerto José Martí a casas de alquiler y hoteles en La Habana y Varadero",
+            description: "Usted puede solicitar nuestro servicio de recojida al aeropuerto internacional José Martí, La Habana, donde nuestro conductor\
+            le esperará frente a la puerta por donde usted saldrá con un cartel con su nombre para trasladarlo al hospedaje que desee en la ciudad de La Habana\
+             o Varadero, ofrecemos servicios los 7 días de la semana las 24 horas del día. \
+             Lo llevamos hasta la puerta de su hospedaje sin cargos adicionales.",
+            galleryImages: ["img/Traslados aeropuertos/Paseo del Prado.webp", "img/Traslados aeropuertos/grand Anston.webp", 
+                "img/Traslados aeropuertos/manzana Kempinski.webp", "img/Traslados aeropuertos/blau.webp"] // Imágenes de la galería
+        },
+        "info2": {
+            title: "Traslado desde el aeropuerto José Martí a Cienfuegos y sfdsfd Trinidad",
+            description: "Detalles sobre el traslado desde el aeropuerto José Martí a Cienfuegos y Trinidad.",
+            galleryImages: ["img/Traslados aeropuertos/Cienfuegos.webp", "img/Traslados aeropuertos/Viñales.webp", "img/Destinos/Matanzas.webp"], // Imágenes de la galería
+            
+        },
+        "info3": {
+            title: "Traslado desde el aeropuerto José Martí a Viñales",
+            description: "Detalles sobre el traslado desde el aeropuerto José Martí a Viñales.",
+            galleryImages: ["img/Traslados aeropuertos/Cienfuegos.webp", "img/Traslados aeropuertos/Viñales.webp", "img/Destinos/Matanzas.webp"] // Imágenes de la galería
+
+        },
+        "info4": {
+            title: "Traslado desde el aeropuerto Juan Gualberto Gómez a La Habana y Varadero",
+            description: "Detalles sobre el traslado desde el aeropuerto Juan Gualberto Gómez a casas de alquiler y hoteles en La Habana y Varadero.",
+            
+        },
+        "info5": {
+            title: "Traslado desde el aeropuerto Juan Gualberto Gómez a Cienfuegos",
+            description: "Detalles sobre el traslado desde el aeropuerto Juan Gualberto Gómez a Cienfuegos.",
+            
+        }
+        
+    };
+
+    const infoTours = {
+        "info1": {
+            title: "Tour de un día desde Varadero hasta la Habana",
+            description: "Le invitamos a recorrer La Habana en nuestros taxis",
+             description2: "Hora de salida 8am",
+             
+            galleryImages: ["img/Traslados aeropuertos/Paseo del Prado.webp", "img/Traslados aeropuertos/grand Anston.webp", 
+                "img/Traslados aeropuertos/manzana Kempinski.webp", "img/Traslados aeropuertos/blau.webp"] // Imágenes de la galería
+        },
+        "info2": {
+            title: "Traslado  a Cienfuegos y sfdsfd Trinidad",
+            description: "Detalles sobre el traslado desde el aeropuerto José Martí a Cienfuegos y Trinidad.",
+            description2: "",
+            galleryImages: ["img/Traslados aeropuertos/Cienfuegos.webp", "img/Traslados aeropuertos/Viñales.webp", "img/Destinos/Matanzas.webp"], // Imágenes de la galería
+            
+        },
+        "info3": {
+            title: "Traslado desde el aeropuerto José Martí a Viñales",
+            description: "Detalles sobre el traslado desde el aeropuerto José Martí a Viñales.",
+            description2: "Detalles segundo texto"
+        },
+        "info4": {
+            title: "Traslado desde el aeropuerto Juan Gualberto Gómez a La Habana y Varadero",
+            description: "Detalles sobre el traslado desde el aeropuerto Juan Gualberto Gómez a casas de alquiler y hoteles en La Habana y Varadero.",
+            description2: "",
+            
+            
+        },
+        "info5": {
+            title: "Traslado desde el aeropuerto Juan Gualberto Gómez a Cienfuegos",
+            description: "Detalles sobre el traslado desde el aeropuerto Juan Gualberto Gómez a Cienfuegos.",
+            description2: ""
+            
+        }
+        
+    };
 
 
+    // Mostrar el modal
+    $('.card p a').on('click', function(event) {
+        event.preventDefault();
+        const card = $(this).closest('.card'); // Obtener la tarjeta
+        const isTraslado = card.closest('.galery1').length > 0; // Verificar si está en la galería de traslados
+        const cardIndex = card.index(); // Obtener el índice de la tarjeta
+
+        let infoKey;
+        if (isTraslado) {
+            infoKey = "info" + (cardIndex + 1); // Crear la clave para traslados
+            if (infoTraslados[infoKey]) {
+                $('#modal-title').text(infoTraslados[infoKey].title);
+                $('#modal-description').text(infoTraslados[infoKey].description);
+                $('#modal-description2').text("");
+                $('#modal-description3').text("");
+                $('.modal-gallery').empty();
+                infoTraslados[infoKey].galleryImages.forEach(image => {
+                    $('.modal-gallery').append(`<img src="${image}" alt="Galería imagen" />`);
+                });
+            }
+        } else {
+            infoKey = "info" + (cardIndex + 1); // Crear la clave para tours
+            if (infoTours[infoKey]) {
+                $('#modal-title').text(infoTours[infoKey].title);
+                $('#modal-description').text(infoTours[infoKey].description);
+                $('#modal-description2').text(infoTours[infoKey].description2);
+                $('.modal-gallery').empty();
+                infoTours[infoKey].galleryImages.forEach(image => {
+                    $('.modal-gallery').append(`<img src="${image}" alt="Galería imagen" />`);
+                });
+            }
+        }
+
+        // Mostrar el modal
+        $('#modal').show();
+        
+
+        // Ocultar las tarjetas
+        $('.services').hide();
+    });
+
+    // Cerrar el modal con back
+    $('.btn-back').on('click', function() {
+        $('#modal').css('display', 'none');
+
+        // Mostrar nuevamente las tarjetas al cerrar el modal
+        $('.services').show();
+    });
+
+    $('.close').on('click', function() {
+        $('#modal').css('display', 'none');
+
+        // Mostrar nuevamente las tarjetas al cerrar el modal
+        $('.services').show();
+    });
+
+    $(window).on('click', function(event) {
+        if (event.target.id === 'modal') {
+            $('#modal').css('display', 'none');
+
+            // Mostrar nuevamente las tarjetas al cerrar el modal
+            $('.services').show();
+        }
+    });
+     
 });
